8f8ae31cf8fdb96221190e3b1a7c8fbf6cf5e813
Chih-Sheng (Tommy) Huang <chih.sheng.huang821@gmail.com>
2017-04-27 09:32:51 +0800

    Add files via upload
    Modify the out-of-memory error, when the larger number of testing data occur

--- Classifier/GaussianClassifier.py
+++ Classifier/GaussianClassifier.py
@@ -1,7 +1,5 @@
 import numpy as np
 from numpy import matlib
-
-
 class GaussianClassifier(object):
     def __init__(self, data, label,testdata):
         self.data=data
@@ -29,20 +27,43 @@ class GaussianClassifier(object):
         sigma=self.sigma
         Ny,dim=testdata.shape
         TL=np.unique(label)
-        p=(np.zeros([dim,Ny]))
-
+        '''When yor run a larger number of testing data, the out-of-memory error may occur.
+            Hence, we have to do a partial segmentation for the testing data.
+            In my code, if the number of testing data is larger than 10,000, 
+               the mechanism of partial segmentation would work. 
+        '''
+        if Ny>10000: 
+            P=(np.zeros([Ny,dim])) 
+            epoch=int(np.floor(Ny/10000))
+            for epo in range(epoch):
+                index=range(0+epo*10000,(1+epo)*10000)
+                tmptestdata=testdata[index,:]
+                pp=GaussianClassifier.GaussianClassifier_test_calculatProbability(tmptestdata,mu,sigma,TL) 
+                P[index,:]=pp
+            index=range(index[-1],Ny)
+            tmptestdata=testdata[index,:]
+            pp=GaussianClassifier.GaussianClassifier_test_calculatProbability(tmptestdata,mu,sigma,TL) 
+            P[index,:]=pp                
+        else:
+            P=GaussianClassifier.GaussianClassifier_test_calculatProbability(testdata,mu,sigma,TL)    
+        return P
+    
+    def GaussianClassifier_test_calculatProbability(testdata,mu,sigma,TL):
+        Ny,dim=testdata.shape
+        P=(np.zeros([dim,Ny]))
         for i in range(len(TL)):
             m=mu[i]
             s=sigma[i]
             tmpm=np.subtract(testdata,matlib.repmat(m, Ny, 1))
             tmps=np.linalg.pinv(s)
-            tmp=np.dot(np.dot(tmpm,tmps),np.transpose(tmpm))
+            tmp=np.dot(tmpm,tmps)
+            tmp=np.dot(tmp,np.transpose(tmpm))
             dy=np.diag(tmp)
             tmp=np.exp(-dy/2)  
             tmp=tmp*(np.linalg.det(s)**(-0.5))*((2*np.pi)**(-dim/2))
-            p[:][i]=(tmp)
+            P[:][i]=(tmp)
         p1=(np.zeros([1,Ny]))
-        p1=matlib.repmat(np.sum(p, axis=0),dim,1)
-        p=p/p1       
-        p=np.transpose(p)
-        return p
+        p1=matlib.repmat(np.sum(P, axis=0),dim,1)
+        P=P/p1       
+        P=np.transpose(P)
+        return P
